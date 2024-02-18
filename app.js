858d6fa6d62bce6d5a3389ab707d185a7d472907
Rahil Vahora <104057403+rahil1202@users.noreply.github.com>
2024-02-18 13:52:20 +0530

    Updated app.js, added auth.controller.js, authJWT.js, user.js, user.js, and modified package-lock.json

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require("express"),
-  app = express();
+  app = express(),
+  mongoose = require("mongoose"),
+  userRoutes = require("./routes/user");
 
-require("dotenv")
-  .config();
+//Connect to database
+try {
+  mongoose.connect("mongodb://localhost:27017/usersdb", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  });
+  console.log("connected to db");
+} catch (error) {
+  handleError(error);
+}
+process.on('unhandledRejection', error => {
+  console.log('unhandledRejection', error.message);
+});
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -10,6 +23,9 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
+
+//using user route
+app.use(userRoutes);
 
 //setup server to listen on port 8080
 app.listen(process.env.PORT || 8080, () => {
